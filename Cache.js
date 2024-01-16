83a9e8a12cccd8212d5da06c037700c877756a29
Taisuke Fukuno <fukuno@jig.jp>
2024-01-16 18:07:21 +0800

    use IndexedDB

--- Cache.js
+++ Cache.js
@@ -1,21 +1,43 @@
-const path = "temp/cache";
+import { IndexedDB } from "https://code4fukui.github.io/IndexedDB/IndexedDB.js";
 
-await Deno.mkdir(path, { recursive: true });
-
-export const fetchJSON = async (url, key) => {
-  const fn = path + "/" + key + ".json";
-  try {
-    const p = await Deno.readTextFile(fn);
-    if (p) {
-      return JSON.parse(p);
-    }
-  } catch (e) {
+export class Cache {
+  constructor(db) {
+    this.db = db;
   }
-  const json = await (await fetch(url)).json();
-  await Deno.writeTextFile(fn, JSON.stringify(json));
-  return json;
+  static async create(dbname = "cache", dbver = 1) {
+    const db = await IndexedDB.create(dbname, ["url", "data"], dbver);
+    return new Cache(db);
+  }
+  async fetchOrLoad(url) {
+    const res = await this.db.get("url", url);
+    if (res.length > 0) {
+      return res[0].data;
+    }
+    const res2 = new Uint8Array(await (await fetch(url)).arrayBuffer());
+    await this.db.add({ url, data: res2 });
+    return res2;
+  }
+  async clear() {
+    const length = await this.db.length();
+    for (let i = 0; i < length; i++) {
+      await this.db.removeAt(i + 1);
+    }
+  }
+}
+
+let cache = null;
+
+export const fetchOrLoad = async (url) => {
+  if (!cache) {
+    cache = await Cache.create();
+  }
+  const bin = await cache.fetchOrLoad(url);
+  return bin;
 };
 
-export const Cache = {
-  fetchJSON,
+export const clearCache = async () => {
+  if (!cache) {
+    cache = await Cache.create();
+  }
+  await cache.clear();
 };
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Cache
- 
+# Cache.js
+
+cache for WebApps with [IndexedDB](https:/github.com/code4fukui/IndexedDB)
+
+## Usage of Cache / fetchOrLoad
+
+```js
+import { fetchOrLoad } from "https://code4fukui.github.io/IndexedDB/Cache.js";
+
+const bin = await fetchOrLoad("./somedata.bin");
+```
